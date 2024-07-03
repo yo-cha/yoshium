7feb6514cfe7a0667a61cab0cb5022e7d0700e10
yoshiko <kabu.yoshiko@gmail.com>
2024-07-03 13:00:17 +0900

    不要部分削除

--- yoshium/yoshium.py
+++ yoshium/yoshium.py
@@ -1,6 +1,5 @@
 import os
 import signal
-
 import requests
 import selenium.webdriver.support.select
 from webdriver_manager.chrome import ChromeDriverManager
@@ -8,9 +7,6 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import time
 
