47eac88a0e04a64f83c086d380e1f27443c88a30
yoshiko <kabu.yoshiko@gmail.com>
2024-07-13 15:10:05 +0900

    バージョン番号修正

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='yoshium',
-    version='0.1',
-    packages=find_packages()
+    version='0.2.0',
+    packages=find_packages(),
+    requires=["requests", "selenium", "ChromeDriverManager", "bs4"],
+    author="yo_charm"
 )
